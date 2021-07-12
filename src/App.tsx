75954bb8efa66fe58907e7f0ece6d0c7b1ec207c
Vitor <vitorafael.r.c@gmail.com>
2021-07-12 14:32:49 -0300

    💡 Componentizando

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,9 @@ import './styles/global.scss';
 
 import './styles/sidebar.scss';
 import './styles/content.scss';
+import { SideBar } from './components/SideBar';
+import { Content } from './components/Content';
+import { Header } from './components/Header';
 
 interface GenreResponseProps {
   id: number;
@@ -54,40 +57,19 @@ export function App() {
     })
   }, [selectedGenreId]);
 
-  function handleClickButton(id: number) {
-    setSelectedGenreId(id);
-  }
-
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
-      <nav className="sidebar">
-        <span>Watch<p>Me</p></span>
-
-        <div className="buttons-container">
-          {genres.map(genre => (
-            <Button
-              key={String(genre.id)}
-              title={genre.title}
-              iconName={genre.name}
-              onClick={() => handleClickButton(genre.id)}
-              selected={selectedGenreId === genre.id}
-            />
-          ))}
-        </div>
-
-      </nav>
+      <SideBar 
+        genres={genres} 
+        selectedGenreId={selectedGenreId} 
+        setSelectedGenreId={setSelectedGenreId} 
+      />
 
       <div className="container">
-        <header>
-          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
-        </header>
+        <Header title={selectedGenre.title} />
 
         <main>
-          <div className="movies-list">
-            {movies.map(movie => (
-              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
-            ))}
-          </div>
+          <Content movies={movies} />
         </main>
       </div>
     </div>
